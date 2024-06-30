c8ceac86cceaf3ba75aa619d1c9c0063066051b1
athena272 <guilhermera272@gmail.com>
2024-06-30 16:09:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from 'paginas/Home';
 import VitrineRestaurantes from 'paginas/VitrineRestaurantes';
 import AdministracaoRestaurantes from 'paginas/Administracao/Restaurantes';
+import FormRestaurante from 'paginas/Administracao/Restaurantes/FormRestaurante';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
       <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
+      <Route path="/admin/restaurantes/novo" element={<FormRestaurante />} />
     </Routes>
   );
 }
