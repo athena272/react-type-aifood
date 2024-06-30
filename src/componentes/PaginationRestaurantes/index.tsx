8ae7ef3837e630d9335842dfc14138bfdac584b6
athena272 <guilhermera272@gmail.com>
2024-06-30 17:15:47 -0300

    Create index.tsx

---
+++ src/componentes/PaginationRestaurantes/index.tsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import IRestaurante from 'interfaces/IRestaurante';
+import IPaginacao from 'interfaces/IPaginacao';
+import style from './PaginationRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const PaginationRestaurantes = () => {
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [nextLinkPage, setNextLinkPage] = useState('')
+
+  useEffect(() => {
+    //obter restaurantes
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+      .then(res => {
+        setRestaurantes(res.data.results)
+        setNextLinkPage(res.data.next)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+  const seeMorePages = () => {
+    axios.get<IPaginacao<IRestaurante>>(nextLinkPage)
+      .then(res => {
+        setRestaurantes(prevState => [...prevState, ...res.data.results])
+        setNextLinkPage(res.data.next)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {
+      restaurantes?.map(item =>
+        <Restaurante restaurante={item} key={item.id} />
+      )
+    }
+    {
+      nextLinkPage &&
+      <button
+        onClick={seeMorePages}
+      >
+        Ver mais
+      </button>
+    }
+  </section>)
+}
+
+export default PaginationRestaurantes
