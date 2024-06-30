2fb81176c6ccc6587f679e04f132f067dddba211
athena272 <guilhermera272@gmail.com>
2024-06-30 15:06:43 -0300

    Update index.tsx

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,26 +1,52 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import IRestaurante from '../../interfaces/IRestaurante';
+import IRestaurante from 'interfaces/IRestaurante';
+import IPaginacao from 'interfaces/IPaginacao';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [nextLinkPage, setNextLinkPage] = useState('')
 
   useEffect(() => {
     //obter restaurantes
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(res => {
         setRestaurantes(res.data.results)
+        setNextLinkPage(res.data.next)
       })
       .catch(err => {
         console.log(err);
       })
   }, [])
 
+  const seeMorePages = () => {
+    axios.get<IPaginacao<IRestaurante>>(nextLinkPage)
+      .then(res => {
+        setRestaurantes(prevState => [...prevState, ...res.data.results])
+        setNextLinkPage(res.data.next)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {
+      restaurantes?.map(item =>
+        <Restaurante restaurante={item} key={item.id} />
+      )
+    }
+    {
+      nextLinkPage &&
+      <button
+        onClick={seeMorePages}
+      >
+        Ver mais
+      </button>
+    }
   </section>)
 }
 
