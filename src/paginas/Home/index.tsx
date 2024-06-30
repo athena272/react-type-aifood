f88389ff37e4552987f34ca22b48fe9ed8f25938
athena272 <guilhermera272@gmail.com>
2024-06-30 15:06:31 -0300

    Update index.tsx

--- src/paginas/Home/index.tsx
+++ src/paginas/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Banner from '../../componentes/Banner';
-import NavBar from '../../componentes/NavBar';
-import Rodape from '../../componentes/Rodape';
+import Banner from 'componentes/Banner';
+import NavBar from 'componentes/NavBar';
+import Rodape from 'componentes/Rodape';
 import estilos from './Home.module.scss';
 
 function App() {
