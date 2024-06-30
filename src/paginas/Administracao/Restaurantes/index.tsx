bdbb9c85caa65f3cf4e9e0da9aeeed18adcba27c
athena272 <guilhermera272@gmail.com>
2024-06-30 16:09:50 -0300

    Update index.tsx

--- src/paginas/Administracao/Restaurantes/index.tsx
+++ src/paginas/Administracao/Restaurantes/index.tsx
@@ -1,7 +1,38 @@
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+import axios from 'axios'
+import IRestaurante from "interfaces/IRestaurante"
+import { useEffect, useState } from "react"
+
 export default function AdministracaoRestaurantes() {
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+            .then(res => setRestaurantes(res.data))
+    }, [])
+
     return (
-        <div>
-            
-        </div>
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        restaurantes.map(restaurante => (
+                            <TableRow key={restaurante.id}>
+                                <TableCell>
+                                    {restaurante.nome}
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
