f77822930cf7930b738bd7cb3fdcd58d1ae9ca38
athena272 <guilhermera272@gmail.com>
2024-06-30 17:56:18 -0300

    Update index.tsx

--- src/paginas/Administracao/Restaurantes/FormRestaurante/index.tsx
+++ src/paginas/Administracao/Restaurantes/FormRestaurante/index.tsx
@@ -1,18 +1,37 @@
 import { TextField, Button } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import IRestaurante from "interfaces/IRestaurante";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function FormRestaurante() {
+    const params = useParams()
     const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+    useEffect(() => {
+        if (params.id) {
+            axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${params.id}/`)
+                .then(res => setNomeRestaurante(res.data.nome))
+        }
+    }, [params])
 
     const onSubmitForm = (ev: React.FormEvent<HTMLFormElement>) => {
         ev.preventDefault()
 
-        axios.post('http://localhost:8000/api/v2/restaurantes/', {
-            nome: nomeRestaurante
-        })
-            .then(() => alert(`Restaurante ${nomeRestaurante} cadastrado com sucesso!`))
+        if (params.id && nomeRestaurante) {
+            axios.put(`http://localhost:8000/api/v2/restaurantes/${params.id}/`, {
+                nome: nomeRestaurante
+            })
+                .then(() => alert(`Restaurante ${nomeRestaurante} atualizado com sucesso!`))
+        }
+        else {
+            axios.post('http://localhost:8000/api/v2/restaurantes/', {
+                nome: nomeRestaurante
+            })
+                .then(() => alert(`Restaurante ${nomeRestaurante} cadastrado com sucesso!`))
+        }
     }
+
 
     return (
         <form onSubmit={onSubmitForm}>
