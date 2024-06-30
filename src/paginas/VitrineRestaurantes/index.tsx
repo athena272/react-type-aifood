ca90dadb05ad993e0898f1fff847285e0da25ffa
athena272 <guilhermera272@gmail.com>
2024-06-30 17:27:08 -0300

    Update index.tsx

--- src/paginas/VitrineRestaurantes/index.tsx
+++ src/paginas/VitrineRestaurantes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from 'componentes/Banner';
 import ListaRestaurantes from 'componentes/ListaRestaurantes';
+// import PaginationRestaurantes from 'componentes/PaginationRestaurantes';
 import NavBar from 'componentes/NavBar';
 import Rodape from 'componentes/Rodape';
 
