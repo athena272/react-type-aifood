c22b365052fd83332a21bc74f1434e78a1aa61df
athena272 <guilhermera272@gmail.com>
2024-06-30 17:15:28 -0300

    Update index.tsx

--- src/paginas/Administracao/Restaurantes/FormRestaurante/index.tsx
+++ src/paginas/Administracao/Restaurantes/FormRestaurante/index.tsx
@@ -1,4 +1,5 @@
 import { TextField, Button } from "@mui/material";
+import axios from "axios";
 import { useState } from "react";
 
 export default function FormRestaurante() {
@@ -6,16 +7,25 @@ export default function FormRestaurante() {
 
     const onSubmitForm = (ev: React.FormEvent<HTMLFormElement>) => {
         ev.preventDefault()
+
+        axios.post('http://localhost:8000/api/v2/restaurantes/', {
+            nome: nomeRestaurante
+        })
+            .then(() => alert(`Restaurante ${nomeRestaurante} cadastrado com sucesso!`))
     }
 
     return (
         <form onSubmit={onSubmitForm}>
             <TextField
-                id="name"
+                id="nome"
+                name="nome"
                 variant="standard"
-                value={nomeRestaurante.value}
+                value={nomeRestaurante}
+                onChange={ev => setNomeRestaurante(ev.target.value)}
+                placeholder="Nome do restaurante"
+                required
             />
-            <Button variant="outlined"></Button>
+            <Button variant="outlined" type="submit">Salvar</Button>
         </form>
     )
 }
