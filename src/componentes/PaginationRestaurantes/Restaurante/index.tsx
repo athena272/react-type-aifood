293b6be4e783bbed75cf1beeb3dda6773602475b
athena272 <guilhermera272@gmail.com>
2024-06-30 23:16:33 -0300

    Update index.tsx

--- src/componentes/PaginationRestaurantes/Restaurante/index.tsx
+++ src/componentes/PaginationRestaurantes/Restaurante/index.tsx
@@ -3,7 +3,7 @@ import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
 import { useState, useEffect } from 'react';
 import IPrato from 'interfaces/IPrato';
-import axios from 'axios';
+import http from 'http/index'
 
 interface RestauranteProps {
   restaurante: IRestaurante
@@ -13,7 +13,7 @@ const Restaurante = ({ restaurante }: RestauranteProps) => {
   const [pratos, setPratos] = useState<IPrato[]>([])
 
   useEffect(() => {
-    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+    http.get<IPrato[]>(`v1/restaurantes/${restaurante.id}/pratos/`)
       .then(res => setPratos(res.data))
   }, [restaurante.id])
 
