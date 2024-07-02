794a351206c283bbacc0d348af8a40b0a49a49a5
athena272 <guilhermera272@gmail.com>
2024-07-01 23:06:58 -0300

    Update index.tsx

--- src/componentes/PaginationRestaurantes/index.tsx
+++ src/componentes/PaginationRestaurantes/index.tsx
@@ -1,23 +1,45 @@
 import { useState, useEffect } from 'react';
 import IRestaurante from 'interfaces/IRestaurante';
 import IPaginacao from 'interfaces/IPaginacao';
-import style from './PaginationRestaurantes.module.scss';
+import styles from './PaginationRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import http from 'http/index'
+import { AxiosRequestConfig } from 'axios';
 
+// esses são os posséveis parâmetros que podemos enviar para a API
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
 
 const PaginationRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [nextLinkPage, setNextLinkPage] = useState('')
   const [previousLinkPage, setPreviousLinkPage] = useState('')
 
-  const loadData = (url: string) => {
-    http.get<IPaginacao<IRestaurante>>(url)
+  const [busca, setBusca] = useState('')
+
+  // agora, o carregarDados recebe opcionalmente as opções de configuração do axios
+  const loadData = (url: string, options: AxiosRequestConfig = {}) => {
+    http.get<IPaginacao<IRestaurante>>(url, options)
       .then(res => {
         setRestaurantes(res.data.results)
         setNextLinkPage(res.data.next)
         setPreviousLinkPage(res.data.previous)
       })
+  }
+  // a cada busca, montamos um objeto de opções
+  const search = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const options = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (busca) {
+      options.params.search = busca
+    }
+    loadData('v1/restaurantes/', options)
   }
 
   useEffect(() => {
@@ -25,8 +47,19 @@ const PaginationRestaurantes = () => {
     loadData('v1/restaurantes/')
   }, [])
 
-  return (<section className={style.ListaRestaurantes}>
+  return (<section className={styles.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={search} className={styles.search__form}>
+      <label htmlFor="buscar">Nome do restaurante</label>
+      <input
+        id='buscar'
+        type="text"
+        placeholder='Insira o nome do restaurante'
+        value={busca}
+        onChange={(ev) => setBusca(ev.target.value)}
+      />
+      <button type='submit'>buscar</button>
+    </form>
     {
       restaurantes?.map(item =>
         <Restaurante restaurante={item} key={item.id} />
