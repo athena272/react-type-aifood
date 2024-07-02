0c3a330a9bc18398ad59bb6bbb142180798381e9
athena272 <guilhermera272@gmail.com>
2024-07-02 01:20:39 -0300

    Update index.tsx

--- src/paginas/Administracao/Restaurantes/FormRestaurante/index.tsx
+++ src/paginas/Administracao/Restaurantes/FormRestaurante/index.tsx
@@ -1,9 +1,8 @@
-import { TextField, Button, Typography, Box, AppBar, Container, Toolbar, Link, Paper } from "@mui/material";
+import { TextField, Button, Typography, Box, Container, Paper } from "@mui/material";
 import http from 'http/index'
 import IRestaurante from "interfaces/IRestaurante";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Link as RouterLink } from "react-router-dom";
 
 export default function FormRestaurante() {
     const params = useParams()
@@ -34,59 +33,36 @@ export default function FormRestaurante() {
     }
 
     return (
-        <>
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar>
-                        <Typography variant="h6">
-                            Administração
-                        </Typography>
-                        <Box sx={{ display: 'flex', flexGrow: 1 }}>
-                            <Link component={RouterLink} to='/admin/restaurantes'>
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Restaurantes
-                                </Button>
-                            </Link>
-                            <Link component={RouterLink} to='/admin/restaurantes/novo'>
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Novo Restaurante
-                                </Button>
-                            </Link>
+        <Box>
+            <Container maxWidth='lg' sx={{ mt: 1 }}>
+                <Paper sx={{ p: 2 }}>
+                    {/* Conteúdo da página */}
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '35px', flexGrow: 1 }}>
+                        <Typography component='h1' variant="h5">Formulário de Restaurantes</Typography>
+                        <Box component='form' onSubmit={onSubmitForm} sx={{ width: '100%', maxWidth: '80%' }}>
+                            <TextField
+                                id="nome"
+                                name="nome"
+                                variant="standard"
+                                value={nomeRestaurante}
+                                onChange={ev => setNomeRestaurante(ev.target.value)}
+                                placeholder="Nome do restaurante"
+                                required
+                                fullWidth
+                                sx={{ marginBottom: '15px', paddingY: '15px' }}
+                            />
+                            <Button
+                                variant="outlined"
+                                type="submit"
+                                fullWidth
+                                sx={{ maxWidth: '200px', p: '10px 15px', m: '0 auto', display: 'block' }}
+                            >
+                                Salvar
+                            </Button>
                         </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-            <Box>
-                <Container maxWidth='lg' sx={{ mt: 1 }}>
-                    <Paper sx={{ p: 2 }}>
-                        {/* Conteúdo da página */}
-                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '35px', flexGrow: 1 }}>
-                            <Typography component='h1' variant="h5">Formulário de Restaurantes</Typography>
-                            <Box component='form' onSubmit={onSubmitForm} sx={{ width: '100%', maxWidth: '80%' }}>
-                                <TextField
-                                    id="nome"
-                                    name="nome"
-                                    variant="standard"
-                                    value={nomeRestaurante}
-                                    onChange={ev => setNomeRestaurante(ev.target.value)}
-                                    placeholder="Nome do restaurante"
-                                    required
-                                    fullWidth
-                                    sx={{ marginBottom: '15px', paddingY: '15px' }}
-                                />
-                                <Button
-                                    variant="outlined"
-                                    type="submit"
-                                    fullWidth
-                                    sx={{ maxWidth: '200px', p: '10px 15px', m: '0 auto', display: 'block' }}
-                                >
-                                    Salvar
-                                </Button>
-                            </Box>
-                        </Box>
-                    </Paper>
-                </Container>
-            </Box>
-        </>
+                    </Box>
+                </Paper>
+            </Container>
+        </Box>
     )
 }
